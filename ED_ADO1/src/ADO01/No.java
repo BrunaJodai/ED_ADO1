65b2af0cf00c4148e1981969fe1ed2f3c2e24791
Bruna Sayuri <bruna_susuke@hotmail.com>
2018-03-08 21:39:22 -0300

    ado

---
+++ ED_ADO1/src/ADO01/No.java
@@ -0,0 +1,31 @@
+package ADO01;
+
+/**
+ *
+ * @author Bruna
+ */
+public class No {
+    private Carro c;
+    private No prox;
+    
+    public No (Carro car){
+        this.c = car;
+        this.prox = null;
+    }
+
+    public Carro getC() {
+        return c;
+    }
+
+    public void setC(Carro c) {
+        this.c = c;
+    }
+
+    public No getProx() {
+        return prox;
+    }
+
+    public void setProx(No prox) {
+        this.prox = prox;
+    }
+}
